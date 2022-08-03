4451618c9e3366c38791f2ee35637096f518ca94
jmart6784 <jmartinez6784@yahoo.com>
2022-08-02 22:54:45 -0700

    Added player name prompt

--- Program.cs
+++ Program.cs
@@ -4,7 +4,16 @@
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello, World!");
+      Player player1 = new Player("Player 1", "X", true, 0);
+      Player player2 = new Player("Player 2", "O", false, 0);
+
+      Console.WriteLine($"{player1.name} please enter your name");
+      player1.name = Console.ReadLine();
+
+      Console.WriteLine($"{player2.name} please enter your name");
+      player2.name = Console.ReadLine();
+
+      Console.WriteLine($"{player1.name} VS {player2.name}");
     }
   }
 }
