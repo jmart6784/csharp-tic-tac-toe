03cd8ae3c4906ac336cf12e5962a886a9362c18a
jmart6784 <jmartinez6784@yahoo.com>
2022-08-06 00:15:55 -0700

    Created TakeTurn() and Place() methods

--- Board.cs
+++ Board.cs
@@ -13,20 +13,45 @@ namespace TicTacToe
     public Board()
     {
       board = new string[,] {
-        {"1", "2", "3"},
-        {"4", "5", "6"},
-        {"7", "8", "9"}
+        {" ", " ", " "},
+        {" ", " ", " "},
+        {" ", " ", " "}
       };
     }
 
     public void PrintBoard()
     {
-      string border = "----------";
-      Console.WriteLine($"{board[0, 0]} | {board[0, 1]} | {board[0, 2]}");
+      string border = "  ----------";
+      Console.WriteLine($"1 {board[0, 0]} | {board[0, 1]} | {board[0, 2]}");
       Console.WriteLine(border);
-      Console.WriteLine($"{board[1, 0]} | {board[1, 1]} | {board[1, 2]}");
+      Console.WriteLine($"2 {board[1, 0]} | {board[1, 1]} | {board[1, 2]}");
       Console.WriteLine(border);
-      Console.WriteLine($"{board[2, 0]} | {board[2, 1]} | {board[2, 2]}");
+      Console.WriteLine($"3 {board[2, 0]} | {board[2, 1]} | {board[2, 2]}");
+      Console.WriteLine("  A   B   C");
+    }
+
+    public void Place(string move, string piece)
+    {
+      string[] moveAry = { move.Substring(0, 1), move.Substring(1, 1) };
+      Array.Sort(moveAry);
+      int row = Int32.Parse(moveAry[0]);
+      string c = moveAry[1];
+      int column = 0;
+
+      if (c == "a")
+      {
+        column = 0;
+      }
+      else if (c == "b")
+      {
+        column = 1;
+      }
+      else if (c == "c")
+      {
+        column = 2;
+      }
+
+      board[row - 1, column] = piece;
     }
   }
 }
--- Program.cs
+++ Program.cs
@@ -4,16 +4,39 @@
   {
     static void Main(string[] args)
     {
+      // 🇽 🅾️ ❌
       Player player1 = new Player("Player 1", "X", true, 0);
       Player player2 = new Player("Player 2", "O", false, 0);
+      Board board = new Board();
 
       // player1.promptName();
       // player2.promptName();
 
-      Board board = new Board();
-      board.PrintBoard();
-
       Console.WriteLine($"{player1.name} VS {player2.name}");
+      board.PrintBoard();
+      TakeTurn(player1, board);
+    }
+
+    static void TakeTurn(Player player, Board board)
+    {
+      string[] legalMoves = {
+        "1a", "1b", "1c", "a1", "b1", "c1",
+        "2a", "2b", "2c", "a2", "b2", "c2",
+        "3a", "3b", "3c", "a3", "b3", "c3",
+      };
+
+      Console.Write($"{player.name} Please take your turn. ");
+      var move = Console.ReadLine();
+
+      if (legalMoves.Contains(move))
+      {
+        board.Place(move, player.piece);
+        board.PrintBoard();
+      }
+      else
+      {
+        TakeTurn(player, board);
+      }
     }
   }
 }
